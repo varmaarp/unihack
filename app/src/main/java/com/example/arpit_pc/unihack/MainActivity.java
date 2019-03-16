63626f6906f1b31af1cc517166921d5c3cbfc964
Arpit Varma <ArpitVarma11@gmail.com>
2019-03-16 14:29:32 +1100

    navigation working from main page to list page

--- app/src/main/java/com/example/arpit_pc/unihack/MainActivity.java
+++ app/src/main/java/com/example/arpit_pc/unihack/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.arpit_pc.unihack;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ShoppingListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
